c9b5e9214824090eaa19592fbccc599899bb6831
Albert Chu <albertzyk@gmail.com>
2013-11-06 16:13:28 +0800

    模板 增加改导航栏title类别

---
+++ layout_UINavigationController/HMProject/HMProject/Categories/UI/UINavigationBar+TitleTextColor.h
@@ -0,0 +1,13 @@
+//
+//  UINavigationBar+TitleTextColor.h
+//  Layout_Nav_Demo
+//
+//  Created by Albert Chu on 13-11-5.
+//  Copyright (c) 2013年 Albert Chu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationBar (TitleTextColor)
+
+@end
